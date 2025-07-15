436ffb27c311393b30a9cb7e80cfd5e7f9d72b7f
Meet Shah <meetushah999@gmail.com>
2025-07-15 18:24:55 +0530

    changed env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
+    if (phase == PHASE_DEVELOPMENT_SERVER) {
         return {
             env: {
                 mongodb_database: 'blog-app-dev'
