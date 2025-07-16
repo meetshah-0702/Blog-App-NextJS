941a576c66d4158624c7bcd4094e4c0c9ad68134
Meet Shah <meetushah999@gmail.com>
2025-07-16 10:37:34 +0530

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -4,16 +4,14 @@ module.exports = (phase) => {
     if (phase == PHASE_DEVELOPMENT_SERVER) {
         return {
             env: {
-                MONGO_URI: `mongodb+srv://root:manager@cluster0.2cokqwg.mongodb.net/${process.env.mongodb_database}`,
-                mongodb_database: 'blog-app-dev'
+                MONGO_URI: `mongodb+srv://root:manager@cluster0.2cokqwg.mongodb.net/blog-app-dev`
             }
         };
     }
 
     return {
         env: {
-            MONGO_URI: `mongodb+srv://root:manager@cluster0.2cokqwg.mongodb.net/${process.env.mongodb_database}`,
-            mongodb_database: 'blog-app'
+            MONGO_URI: `mongodb+srv://root:manager@cluster0.2cokqwg.mongodb.net/blogs-app`
         }
     };
 };
